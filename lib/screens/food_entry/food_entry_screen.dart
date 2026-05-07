9ab65fa0bb406d474975a5fbc1bfcf76d884a696
iamarchitshah <d24it168@charusat.edu.in>
2026-05-07 14:42:57 +0530

    commit 3

--- lib/screens/food_entry/food_entry_screen.dart
+++ lib/screens/food_entry/food_entry_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:uuid/uuid.dart';
+import 'package:nutriplan_ai/core/theme/app_theme.dart';
 import 'package:nutriplan_ai/data/models/meal.dart';
 import 'package:nutriplan_ai/providers/meal_provider.dart';
 
